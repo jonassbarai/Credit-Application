8b873b9f42ecffa6532b021d50fcf987788b9f6c
jonassbarai <jonas_sbarai@hotmail.com>
2023-06-08 19:34:30 -0300

    Configurações do banco de dados H2 e anotações de criação das tabelas

--- src/main/kotlin/me/dio/creditapplicationsystem/entity/Adress.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/entity/Adress.kt
@@ -1,5 +1,9 @@
 package me.dio.creditapplicationsystem.entity
 
+import jakarta.persistence.Embeddable
+import jakarta.persistence.Embedded
+
+@Embeddable
 data class Adress(
     var zipCode: String = "",
     var street: String =""
